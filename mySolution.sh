f30b5b3f879f9affaa575bfa37e79bc6bc4da781
Pranit Abraham Thomas <109825570+PranitThomas@users.noreply.github.com>
2024-09-15 14:39:19 +0530

    Update mySolution.sh

--- mySolution.sh
+++ mySolution.sh
@@ -1,5 +1,13 @@
-SOLUTION_FILE="SOLUTION"
-SECRET=$(cat "$SOLUTION_FILE")
-echo "The secret is: $SECRET"
-#test
-
+#!/bin/bash
+wget https://exit-zero-academy.github.io/DevOpsTheHardWayAssets/linux_project/secretGenerator.tar.gz
+gunzip secretGenerator.tar.gz
+tar -xvf secretGenerator.tar
+cd src
+rm -rf maliciousFiles
+mkdir secretDir
+cd secretDir
+touch .secret
+chmod 600 .secret
+cd ..
+chmod +x generateSecret.sh
+./generateSecret.sh
